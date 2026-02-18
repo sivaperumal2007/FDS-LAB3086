dd55b9ef7e1e084ff4112a8c29b299649fb99ced
sivaperumal2007 <chokkanathan7094@gmail.com>
2026-02-18 13:27:06 +0530

    EXNO1FDS.py

---
+++ program1FDS.py
@@ -0,0 +1,49 @@
+EX NO:1A
+import numpy as np
+
+arr = np.array([[1, 2, 3],
+                [4, 2, 5]])
+
+
+print("Array is of type:", type(arr))
+
+
+print("No. of dimensions:", arr.ndim)
+
+print("Shape of array:", arr.shape)
+
+
+print("Size of array:", arr.size)
+
+
+print("Array stores elements of type:", arr.dtype)
+
+
+EX NO: 1B
+import numpy as np 
+a = np.array([[1,2,3],[3,4,5],[4,5,6]]) 
+print(a) 
+print("After slicing") 
+print(a[1:])
+
+EX NO:1C
+
+import numpy as np
+
+a = np.array([[1, 2, 3],
+              [3, 4, 5],
+              [4, 5, 6]])
+
+print('Our array is:')
+print(a)
+
+print('The items in the second column are:')
+print(a[..., 1])
+print('\n')
+
+print('The items in the second row are:')
+print(a[1, ...])
+print('\n')
+
+print('The items column 1 onwards are:')
+print(a[..., 1:])
