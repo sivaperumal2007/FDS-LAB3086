6ead451f05a6b83f41cba206a665bdd78a9a76b6
sivaperumal2007 <chokkanathan7094@gmail.com>
2026-03-11 14:16:47 +0530

    ex no12program.py

---
+++ ex no12program.py
@@ -0,0 +1,48 @@
+from dateutil.parser import parse
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+# Plot settings
+plt.rcParams.update({'figure.figsize': (10, 7), 'figure.dpi': 120})
+
+# Import dataset as DataFrame
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/selva86/datasets/master/a10.csv",
+    parse_dates=['date']
+)
+
+print(df.head())
+
+# Dataset source example
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/selva86/datasets/master/MarketArrivals.csv"
+)
+
+# Filter only MUMBAI market
+df = df.loc[df.market == 'MUMBAI', :]
+print(df.head())
+
+# Time series dataset
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/selva86/datasets/master/a10.csv",
+    parse_dates=['date'],
+    index_col='date'
+)
+
+# Function to draw plot
+def plot_df(df, x, y, title="", xlabel='Date', ylabel='Value', dpi=100):
+    plt.figure(figsize=(16,5), dpi=dpi)
+    plt.plot(x, y, color='tab:red')
+    plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
+    plt.show()
+
+# Call function
+plot_df(
+    df,
+    x=df.index,
+    y=df.value,
+    title="Monthly anti-diabetic drug sales in Australia from 1992 to 2008"
+)
